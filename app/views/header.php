c8327ea0a8db92b856c343630dba453f95b38b42
remythemsen <iam@stoked.dk>
2014-05-19 18:29:14 +0200

    Remove breadcrumbs div

--- app/views/header.php
+++ app/views/header.php
@@ -4,9 +4,7 @@ require_once(__SITE_PATH.'app/views/head.php');
 <body>
     <?php require_once(__SITE_PATH.'app/views/nav.php'); ?>
     <div class="row" id="header">
-        <div id="breadcrumbs">
-        </div>
     </div>
-    
+
     <div class="row" id="content">
         <?php Notifier::printAll(); ?>
