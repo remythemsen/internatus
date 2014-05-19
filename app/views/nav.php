005baaa6a987dc4b18abce679b5afe0cd457d63e
remythemsen <iam@stoked.dk>
2014-05-19 19:11:39 +0200

    Removed unused menu items

--- app/views/nav.php
+++ app/views/nav.php
@@ -6,16 +6,8 @@
         <nav class="eight columns navbar">
             <?php if(isset($this->account)) : ?>
             <ul id="main-menu">
-                <li><a href="<?php echo BASE_URL; ?>">Dashboard</a></li>
-                <li id="user-menu-item">
-                    <a href="<?php echo BASE_URL; ?>account/settings"><?php echo ucfirst(Auth::account('username')); ?><i class="icon-down-open"></i></a>
-                    <div class="dropdown">
-                        <ul>
-                            <li><a href="<?php echo BASE_URL; ?>account/settings"><i class="icon-cog"></i>Settings</a></li>
-                            <li><a href="<?php echo BASE_URL; ?>account/logout"><i class="icon-logout"></i>Logout</a></li>
-                        </ul>
-                    </div>
-                </li>
+                
+
             <ul>
             <?php endif; ?>
         </nav>
