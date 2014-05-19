3c9efc2a9d0d0fb1a163362484eaad95a3c9a65f
remythemsen <iam@stoked.dk>
2014-05-19 22:17:16 +0200

    Add ability to check for account and email availability

--- app/helpers/Validator.php
+++ app/helpers/Validator.php
@@ -1,45 +1,75 @@
 <?php
 
 class Validator {
-    public static function check($string, $type) {
+    public static function check($array) {
+
+        // Accepts Associate Array, keys: username, password, email.
 
         $errors = array();
+        $db = new Database();
 
-        // check for empty string
-        if(empty($string)) {
-            array_push($errors, 'the '.$type.' field is required');
-        }
 
-        if($type == 'email') {
+        if(array_key_exists('email', $array)) {
             // check for type: email
-            if(!filter_var($string, FILTER_VALIDATE_EMAIL)) {
+
+            if(empty($array['email'])) {
+                array_push($errors, 'Email field is required');
+            }
+
+            if(!filter_var($array['email'], FILTER_VALIDATE_EMAIL)) {
                 // invalid address
                 array_push($errors, 'That is not a Valid email address!');
             }
+
+            $stmt = $db->prepare("SELECT email FROM accounts WHERE email = :email LIMIT 1");
+            $stmt->execute(array(':email' => $array['email']));
+
+            // is available
+            if($stmt->rowCount() != 0) {
+                Notifier::add('warning', 'This email address is already associated with an account');
+            }
         }
 
-        if($type == 'password') {
+        if(array_key_exists('password', $array)) {
+
+            // check for empty value
+            if(empty($array['password'])) {
+                array_push($errors, 'Password field is required');
+            }
 
             // check for length
-            if(strlen($string) < 8) {
-                array_push($errors, 'Password is to short');
+            if(strlen($array['password']) < 8) {
+                array_push($errors, 'Password needs to be at least 8 characters long');
             }
 
             // check if has number
-            if(!preg_match("#[0-9]+#", $string)) {
+            if(!preg_match("#[0-9]+#", $array['password'])) {
                 array_push($errors, 'Password must include at least one number');
             }
 
             // check if has letter
-            if(!preg_match("#[a-zA-Z]+#", $string)) {
+            if(!preg_match("#[a-zA-Z]+#", $array['password'])) {
                 array_push($errors, 'Password must include at least one letter');
             }
         }
 
-        if($type == 'username') {
+        if(array_key_exists('username', $array)) {
+
+            // check for empty value
+            if(empty($array['username'])) {
+                array_push($errors, 'the Username field is required');
+            }
             // check for length
-            if(strlen($string) < 3) {
+            if(strlen($array['username']) < 3) {
                 array_push($errors, 'Username has to be at least 3 characters long.');
+            }
+
+            $stmt = $db->prepare("SELECT username FROM accounts WHERE username = :username LIMIT 1");
+            $stmt->execute(array(':username' => $array['username']));
+
+            // is available
+            if($stmt->rowCount() != 0) {
+                Notifier::add('warning', 'Username has already been taken');
             }
         }
 
@@ -49,5 +79,8 @@ class Validator {
         } else {
             Notifier::add('warning', implode(', ', $errors));
         }
+
+        $db = null;
     }
+
 }
--- app/models/viewmodels/AccountViewModel.php
+++ app/models/viewmodels/AccountViewModel.php
@@ -3,59 +3,22 @@
 class AccountViewModel extends ViewModel {
 
     public function create() {
-
-        // TODO: Remove the usernames, and replace with Email.
-
-        $username = (isset($_POST['username']) ? trim($_POST['username']) : false);
-        $email = (isset($_POST['email']) ? trim($_POST['email']) : false);
-        $password = (isset($_POST['password']) ? trim($_POST['password']) : false);
-        
-        // checking for all required data
-        if($username && $email && $password) {
-
-            // check is username already exists:
-            $statement = $this->db->prepare("SELECT username FROM accounts WHERE username = ? LIMIT 1");
-            // execute
-            $statement->execute(array($username));
-            if ( $statement->rowCount() == 0 ) {
-                // prepared statement
-                $statement = $this->db->prepare("INSERT INTO accounts (username, password, email, is_admin, is_active) VALUES (?, ?, ?, ?, ?)");
-
-                // execute statement
-                $statement->execute(array($username, Hash::make($password), $email, 0, 1));
-
-                Notifier::add('success', 'Congratulations, your account has been created, now login with your new credentials.');
-                return true;
-                exit();
-
-            } else {
-                Notifier::add('warning', 'Account name has already been taken');
-                return false; 'Account name is already taken';
-                exit();
-            }
-        } else {
-            Notifier::add('warning', 'All fields are required');
-            return false;
-            exit();
-        }
-    }
-    public function create2() {
         // get + trim vars
         $username = (isset($_POST['username']) ? trim($_POST['username']) : false);
         $email = (isset($_POST['email']) ? trim($_POST['email']) : false);
         $password = (isset($_POST['password']) ? trim($_POST['password']) : false);
 
+        // Validation
 
-        // check for no empty fields
-
-        // check email input
-
-
-        // check password input
-        // check username && email available
-        // modify password with salt(s)
-        // insert into accounts
-
-        // handle return + output errors
+        if(Validator::check(array(
+            'username' => $username,
+            'password' => $password,
+            'email' => $email
+        ))) {
+            $stmt = $this->db->prepare("INSERT INTO accounts (username, password, email, is_admin, is_active) VALUES (?, ?, ?, ?, ?)");
+            $stmt->execute(array($username, Hash::make($password), $email, 0, 1));
+            Notifier::add('success', 'Congratulations, your account has been created, now login with your new credentials.');
+            return true;
+        }
     }
 }
