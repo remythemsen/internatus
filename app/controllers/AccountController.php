7372308473c08cfacf15edaa35feb532463a7029
remythemsen <iam@stoked.dk>
2014-05-21 11:23:18 +0200

    remove account view model; add create method to account controller

--- app/controllers/AccountController.php
+++ app/controllers/AccountController.php
@@ -36,8 +36,24 @@ class AccountController extends Controller {
 
     function create_do() {
         if(!Auth::check()) {
-            $this->model->create();
-            URL::redirect('account/login');
+            // get + trim vars
+            $username = (isset($_POST['username']) ? trim($_POST['username']) : false);
+            $email = (isset($_POST['email']) ? trim($_POST['email']) : false);
+            $password = (isset($_POST['password']) ? trim($_POST['password']) : false);
+
+            // Validation
+
+            if(Validator::check(array(
+                'username' => $username,
+                'password' => $password,
+                'email' => $email
+            ))) {
+
+                $stmt = $this->db->prepare("INSERT INTO accounts (username, password, email, is_admin, is_active) VALUES (?, ?, ?, ?, ?)");
+                $stmt->execute(array($username, Hash::make($password), $email, 0, 1));
+                Notifier::add('success', 'Congratulations, your account has been created, now login with your new credentials.');
+                URL::redirect('account/login');
+            }
         } else {
             URL::redirect('home');
         }
