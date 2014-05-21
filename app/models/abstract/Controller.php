5de2a54b38206096348c3a1755c5e47d75a8c9b6
remythemsen <iam@stoked.dk>
2014-05-21 11:17:27 +0200

    remove autoloading of viewmodel

--- app/models/abstract/Controller.php
+++ app/models/abstract/Controller.php
@@ -12,30 +12,9 @@ Abstract Class Controller {
         // storing the global registry.
         $this->registry = $registry;
 
-        // get the current active controller name
-        $controllerName = preg_replace('/Controller$/', '', get_class($this));
-
-        // path to the view models
-        $path = __SITE_PATH.'app/models/viewmodels/';
-
-        // complete path with filename.
-        $file = $path.$controllerName.'ViewModel.php';
-
-        // including the needed viewmodel file.
-        if(file_exists($file)) {
-            include $file;
-
-            $viewModel = $controllerName.'ViewModel';
-            // storing new instance of the viewmodel in this.
-            $this->model = new $viewModel($this->registry);
-
-        }
-                        
         // creating a new view object
         $this->view = new View($this->registry);
         
-        
-
     }
     
     // Force the index method for all controllers
