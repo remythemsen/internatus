e2733523c3f4c6e2454697c3e4e6ad144ad0601e
remythemsen <iam@stoked.dk>
2014-06-19 17:01:23 +0200

    Removing unused code block

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -4,19 +4,6 @@ use TheWall\Core\Helpers;
 
 class HomeController extends Controller {
     function getIndex() {
-
-        // Retrieve All posts and pass to view
-        $this->view->posts = PostQuery::create()->orderById('desc')->find();
-
-        /*
-
-        MESSAGES DISABLED
-
-        $this->view->messages = MessageQuery::create()
-                                    ->filterByReceiverId(Helpers\Session::get('user_id'))
-                                    ->orderById('desc')
-                                    ->find();
-        */
         // Render the view.
         $this->view->render('home/index');
 
